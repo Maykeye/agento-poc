6bba247ab57ced37297a9a6f346f9bad619dfe87
Maykeye <Maykeye@users.noreply.github.com>
2026-03-15 02:07:46 +0600

    Preparing world gen

--- main.py
+++ main.py
@@ -48,7 +48,27 @@ class AgencyNode:
 def main():
     initial = read_text("./prompts/initial-game-idea.txt")
     node = AgencyNode()
-    node.simple(initial)
+    node.simple(
+        data_tag("PAST_ORIGINAL_TASK", initial)
+        + "\n"
+        + data_tag(
+            "IMPLEMENT_TASK",
+            """
+Preparation:
+* Read the current DESIGN.md to know what project about and common style for e.g. testsing.
+
+Main Tasks:
+* Generate `world.rs` module and include it from main.rs
+    * Make struct World that consist of world chunks (chunk pos -> WorldChunk)
+    * WorldChunk contains information about terrain (ore type and its current health).
+    * Create only structures for World/WorldChunk. No need for code yet.
+    * Create src/_tests/test_world.rs. 
+        * Create test_dummy now that just `assert_eq!(1,2)`
+* DESIGN.md has draft version of `struct World` without mention of chunk. Once you'll implement source c 
+
+            """,
+        )
+    )
 
 
 if __name__ == "__main__":
